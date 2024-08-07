28d96041410eab60c7f3e258737ceebfe3527d2d
mbarshapaul <missprantikapaul@gmail.com>
2024-08-07 15:49:27 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane'
+});
+new VenoBox({
+    selector: '.my-video-links',
+});
+var containerEl = document.querySelector('.container');
+
+var mixer = mixitup(containerEl);
